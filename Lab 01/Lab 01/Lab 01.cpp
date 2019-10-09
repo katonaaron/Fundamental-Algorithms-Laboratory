b507dba2fd45543f498a464edfdb26baea30034f
Katona Áron <katonaaron01@gmail.com>
2019-10-10 00:57:22 +0300

    All the sorting algorithms and their evaluation were implemented

--- Lab 01/Lab 01/Lab 01.cpp
+++ Lab 01/Lab 01/Lab 01.cpp
@@ -9,12 +9,11 @@ using namespace std;
 #define MIN_SIZE 100
 #define MAX_SIZE 10000
 #define STEPS 500//100
+#define NR_OF_MEASUREMENTS_AVERAGE_CASE 5
 #define RANGE_MIN 10
 #define RANGE_MAX 50000
 
 //#define DEBUG
-
-Profiler profiler("Direct-Sorting-Methods");
 
 typedef void (*SortingAlgortithm)(int A[], size_t Size, Operation Ass, Operation Comp);
 
@@ -35,116 +34,220 @@ void TestSortingAlgorithm(SortingAlgortithm Sort)
 	const enum { RANDOM = 0, ASC, DESC } order = RANDOM;
 	const bool unique = false;
 
-	static int data[size], data2[size];
+	static int data[size], dataCopy[size];
 
+	Profiler profiler("test");
 	Operation ass = profiler.createOperation("assing_test", size);
 	Operation comp = profiler.createOperation("comp_test", size);
 
 	FillRandomArray(data, size, range_min, range_max, unique, order);
-	memcpy(data2, data, size * sizeof(int));;
+	copy(begin(data), end(data), begin(dataCopy));
+	cout << "dataCopy: ";
+	PrintArray(dataCopy, size);
 	PrintArray(data, size);
 
 	Sort(data, size, ass, comp);
 	PrintArray(data, size);
 
-	sort(data2, data2 + size);
-	cout << "data2: ";
-	PrintArray(data2, size);
-	assert(memcmp(data, data2, size * sizeof(int)) == 0);
+	sort(dataCopy, dataCopy + size);
+	cout << "dataCopy: ";
+	PrintArray(dataCopy, size);
+	assert(memcmp(data, dataCopy, size * sizeof(int)) == 0);
+
+	//profiler.showReport();
 }
 
-void InsertionSort(int A[], size_t Size, const char* AssOp, const char* CompOp)
+void InsertionSort(int A[], size_t Size, Operation Ass, Operation Comp)
 {
 	int key, j;
 	for (int i = 1; i < Size; i++)
 	{
 		key = A[i];
-		profiler.countOperation(AssOp, Size);
+		Ass.count();
 		j = i - 1;
 		while (j >= 0 && A[j] > key)
 		{
-			profiler.countOperation(CompOp, Size);
+			Comp.count();
 			A[j + 1] = A[j];
-			profiler.countOperation(AssOp, Size);
+			Ass.count();
 			j--;
 		}
 		if (j >= 0)
-			profiler.countOperation(CompOp, Size);
-		A[j + 1] = key;
-		profiler.countOperation(AssOp, Size);
+			Comp.count();
+		if (j != i - 1)
+		{
+			A[j + 1] = key;
+			Ass.count();
+		}
 
 #ifdef DEBUG
 		PrintArray(A, Size);
 #endif // DEBUG
+		}
 	}
-}
 
-void SelectionSort(int A[], size_t Size, string AssOp, string CompOp)
+void SelectionSort(int A[], size_t Size, Operation Ass, Operation Comp)
 {
-	int min_index, j, aux;
+	int min_index, j;
 	for (int i = 0; i < Size; i++)
 	{
 		min_index = i;
 		for (j = i + 1; j < Size; j++)
 		{
+			Comp.count();
 			if (A[j] < A[min_index])
 			{
 				min_index = j;
 			}
 		}
+		if (min_index != i)
+		{
+			swap(A[i], A[min_index]);
+			Ass.count(3);
+		}
 
-		swap(A[i], A[min_index]);
 #ifdef DEBUG
 		PrintArray(A, Size);
 #endif // DEBUG
+		}
 	}
-}
+
+void BubbleSort(int A[], size_t Size, Operation Ass, Operation Comp)
+{
+	bool swapped = true;
+	for (int i = 0; i < Size - 1; i++)
+	{
+		swapped = false;
+		for (int j = Size - 1; j > i; j--)
+		{
+			Comp.count();
+			if (A[j] < A[j - 1])
+			{
+				swap(A[j], A[j - 1]);
+				Ass.count(3);
+				swapped = true;
+			}
+		}
+#ifdef DEBUG
+		PrintArray(A, Size);
+#endif // DEBUG
+
+		if (!swapped)
+			return;
+			}
+		}
 
 void EvaluateSortingAlgorithms()
 {
-	static int data[MAX_SIZE];
+	static int data[MAX_SIZE], dataCopy[MAX_SIZE];
 
-	//Best Case - Insertion
+	//Best Case
+	Profiler profiler("Direct-Sorting-Methods-Best-Case");
 	for (unsigned int size = MIN_SIZE; size <= MAX_SIZE; size += STEPS)
 	{
 		cout << "Evaluating best case for size: " << size << "\n";
-		//Operation ass = profiler.createOperation("assing_Insertion_best", size);
-		//Operation comp = profiler.createOperation("comp_Insertion_best", size);
-		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 1);
-		InsertionSort(data, size, "assing_Insertion_best", "comp_Insertion_best");
-	}
-	profiler.addSeries("sum_Insertion_best", "assing_Insertion_best", "comp_Insertion_best");
+		Operation assIns = profiler.createOperation("assing_Insertion", size);
+		Operation assSel = profiler.createOperation("assing_Selection", size);
+		Operation assBub = profiler.createOperation("assing_Bubble", size);
+		Operation compIns = profiler.createOperation("comp_Insertion", size);
+		Operation compSel = profiler.createOperation("comp_Selection", size);
+		Operation compBub = profiler.createOperation("comp_Bubble", size);
 
-	//Worst Case - Insertion
+		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 1);
+		copy(begin(data), end(data), begin(dataCopy));
+
+		cout << "\tInsertion\n";
+		InsertionSort(data, size, assIns, compIns);
+
+		copy(begin(dataCopy), end(dataCopy), begin(data));
+		cout << "\tSelection\n";
+		SelectionSort(data, size, assSel, compSel);
+
+		copy(begin(dataCopy), end(dataCopy), begin(data));
+		cout << "\tBubble\n";
+		BubbleSort(data, size, assBub, compBub);
+	}
+	profiler.addSeries("sum_Insertion", "assing_Insertion", "comp_Insertion");
+	profiler.addSeries("sum_Selection", "assing_Selection", "comp_Selection");
+	profiler.addSeries("sum_Bubble", "assing_Bubble", "comp_Bubble");
+	profiler.createGroup("assing", "assing_Insertion", "assing_Selection", "assing_Bubble");
+	profiler.createGroup("comp_Insertion_Bubble", "comp_Insertion", "comp_Bubble");
+	profiler.createGroup("sum_Insertion_Bubble", "sum_Insertion", "sum_Bubble");
+
+	//Worst Case
+	profiler.reset("Direct-Sorting-Methods-Worst-Case");
 	for (unsigned int size = MIN_SIZE; size <= MAX_SIZE; size += STEPS)
 	{
 		cout << "Evaluating worst case for size: " << size << "\n";
-		//Operation ass = profiler.createOperation("assing_Insertion_worst", size);
-		//Operation comp = profiler.createOperation("comp_Insertion_worst", size);
-		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 2);
-		InsertionSort(data, size, "assing_Insertion_worst", "comp_Insertion_worst");
-	}
-	profiler.addSeries("sum_Insertion_worst", "assing_Insertion_worst", "comp_Insertion_worst");
+		Operation assIns = profiler.createOperation("assing_Insertion", size);
+		Operation assSel = profiler.createOperation("assing_Selection", size);
+		Operation assBub = profiler.createOperation("assing_Bubble", size);
+		Operation compIns = profiler.createOperation("comp_Insertion", size);
+		Operation compSel = profiler.createOperation("comp_Selection", size);
+		Operation compBub = profiler.createOperation("comp_Bubble", size);
 
-	//Average Case - Insertion
-	/*for (unsigned int size = MIN_SIZE; size <= MAX_SIZE; size += STEPS)
-	{
-		Operation assIns = profiler.createOperation("assing_Insertion_average", size);
-		Operation compIns = profiler.createOperation("comp_Insertion_average", size);
-		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 0);
+		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 2);
+		copy(begin(data), end(data), begin(dataCopy));
+
+		cout << "\tInsertion\n";
 		InsertionSort(data, size, assIns, compIns);
-	}
-	profiler.addSeries("sum_Insertion_average", "assing_Insertion_average", "comp_Insertion_average");*/
 
-	/*//Selection sort - no case
-	for (unsigned int size = MIN_SIZE; size <= MAX_SIZE; size += STEPS)
-	{
-		Operation ass = profiler.createOperation("assing_Selection", size);
-		Operation comp = profiler.createOperation("comp_Selection", size);
-		FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 2);
-		InsertionSort(data, size, ass, comp);
+		copy(begin(dataCopy), end(dataCopy), begin(data));
+		cout << "\tSelection\n";
+		SelectionSort(data, size, assSel, compSel);
+
+		copy(begin(dataCopy), end(dataCopy), begin(data));
+		cout << "\tBubble\n";
+		BubbleSort(data, size, assBub, compBub);
 	}
-	profiler.addSeries("sum_Selection", "assing_Selection", "comp_Selection");*/
+	profiler.addSeries("sum_Insertion", "assing_Insertion", "comp_Insertion");
+	profiler.addSeries("sum_Selection", "assing_Selection", "comp_Selection");
+	profiler.addSeries("sum_Bubble", "assing_Bubble", "comp_Bubble");
+	profiler.createGroup("assing_Insertion_Bubble", "assing_Insertion", "assing_Bubble");
+	profiler.createGroup("comp", "comp_Insertion", "comp_Selection", "comp_Bubble");
+	profiler.createGroup("sum", "sum_Insertion", "sum_Selection", "sum_Bubble");
+
+	//Average Case
+	profiler.reset("Direct-Sorting-Methods-Average-Case");
+	for (int m = 1; m <= NR_OF_MEASUREMENTS_AVERAGE_CASE; m++)
+		for (unsigned int size = MIN_SIZE; size <= MAX_SIZE; size += STEPS)
+		{
+			cout << "Evaluating average case for size: " << size << " - measurement: " << m << "\n";
+			Operation assIns = profiler.createOperation("assing_Insertion", size);
+			Operation assSel = profiler.createOperation("assing_Selection", size);
+			Operation assBub = profiler.createOperation("assing_Bubble", size);
+			Operation compIns = profiler.createOperation("comp_Insertion", size);
+			Operation compSel = profiler.createOperation("comp_Selection", size);
+			Operation compBub = profiler.createOperation("comp_Bubble", size);
+
+			FillRandomArray(data, size, RANGE_MIN, RANGE_MAX, false, 0);
+			copy(begin(data), end(data), begin(dataCopy));
+
+			cout << "\tInsertion\n";
+			InsertionSort(data, size, assIns, compIns);
+
+			copy(begin(dataCopy), end(dataCopy), begin(data));
+			cout << "\tSelection\n";
+			SelectionSort(data, size, assSel, compSel);
+
+			copy(begin(dataCopy), end(dataCopy), begin(data));
+			cout << "\tBubble\n";
+			BubbleSort(data, size, assBub, compBub);
+		}
+	profiler.divideValues("assing_Insertion", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.divideValues("assing_Selection", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.divideValues("assing_Bubble", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.divideValues("comp_Insertion", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.divideValues("comp_Selection", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.divideValues("comp_Bubble", NR_OF_MEASUREMENTS_AVERAGE_CASE);
+	profiler.addSeries("sum_Insertion", "assing_Insertion", "comp_Insertion");
+	profiler.addSeries("sum_Selection", "assing_Selection", "comp_Selection");
+	profiler.addSeries("sum_Bubble", "assing_Bubble", "comp_Bubble");
+	profiler.createGroup("assing_Insertion_Bubble", "assing_Insertion", "assing_Bubble");
+	profiler.createGroup("comp", "comp_Insertion", "comp_Selection", "comp_Bubble");
+	profiler.createGroup("sum", "sum_Insertion", "sum_Selection", "sum_Bubble");
+
+	profiler.showReport();
 }
 
 
@@ -153,8 +256,8 @@ int main()
 {
 	//TestSortingAlgorithm(InsertionSort);
 	//TestSortingAlgorithm(SelectionSort);
+	//TestSortingAlgorithm(BubbleSort);
 	EvaluateSortingAlgorithms();
-	profiler.showReport();
 
 	return 0;
 }
