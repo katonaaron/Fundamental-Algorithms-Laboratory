e1634fb1c1aa5d2c8dcceaa32e905ea3310e09d6
Katona Áron <katonaaron01@gmail.com>
2019-11-21 00:31:43 +0200

    Lab 06: Delete, Demo, Evaluation implemented + profiler added + chart generated

--- Lab 06/Lab 06/Lab 06.cpp
+++ Lab 06/Lab 06/Lab 06.cpp
@@ -1,65 +1,90 @@
 #include <iostream>
 #include <cassert>
 #include <time.h>
+#include "Profiler.h"
 
 using namespace std;
 
+//#define DEMO
+
 class OSTree
 {
-    class Node
+    struct Node
     {
-    public:
-        int key;
+        int data;
         Node* left;
         Node* right;
+        Node* parent;
         size_t size;
 
-        Node(int key);
+        Node(int data);
+        ~Node();
     };
 
     Node* root;
 
     OSTree();
-public:
-    static OSTree* BUILD_TREE(int n);
-    int OS_SELECT(int i);
-    ~OSTree();
-    //Demo
-    void Print();
-private:
-    static Node* BuildTreeRec(int minVal, int maxVal);
-    Node* OS_SELECT(OSTree::Node* root, int i);
-    void DeleteTree(Node* root);
+    static Node* BuildPBTFromRange(int range_min, int range_max, Operation op);
+
+    Node* FindMin(Node* node, Operation op);
+    Node* Select(Node* root, int i, Operation op);
+    Node* Delete(Node* node, Operation op);
+    void DecrementSizeOfAncestors(Node* node, Operation op);
+
     //Demo
     void PrintRec(Node* node, int indent);
+public:
+    ~OSTree();
+    static OSTree* Build(int n, Operation op);
+    int Select(int i, Operation op);
+    void Delete(int i, Operation op);
+    size_t Size();
+
+    //Demo
+    void Print();
 };
 
-OSTree::OSTree()
+OSTree::OSTree() : root(nullptr)
 {
-    root = nullptr;
 }
 
-OSTree* OSTree::BUILD_TREE(int n)
+OSTree* OSTree::Build(int n, Operation op)
 {
     if (n < 1)
         throw new invalid_argument("n must be >= 1");
     OSTree* os = new OSTree();
-    os->root = BuildTreeRec(1, n);
+    os->root = BuildPBTFromRange(1, n, op);
+    op.count(2);
     return os;
 }
 
-int OSTree::OS_SELECT(int i)
+int OSTree::Select(int i, Operation op)
 {
     if (root == nullptr || i < 1 || (unsigned int)i > root->size)
         throw new invalid_argument("i must be between 1 and the size of the tree");
-    Node* node = OS_SELECT(root, i);
+    Node* node = Select(root, i, op);
     assert(node != nullptr);
-    return node->key;
+    return node->data;
+}
+
+void OSTree::Delete(int i, Operation op)
+{
+    Node* node = Select(root, i, op);
+    node = Delete(node, op);
+    DecrementSizeOfAncestors(node, op);
+    node->left = node->right = nullptr;
+    delete node;
+}
+
+size_t OSTree::Size()
+{
+    return root == nullptr ? 0 : root->size;
 }
 
 OSTree::~OSTree()
 {
-    DeleteTree(root);
+    if (root != nullptr)
+        delete root;
 }
 
 void OSTree::Print()
@@ -69,52 +94,128 @@ void OSTree::Print()
     cout << "\n";
 }
 
-OSTree::Node* OSTree::BuildTreeRec(int minVal, int maxVal)
+OSTree::Node* OSTree::BuildPBTFromRange(int range_min, int range_max, Operation op)
 {
-    if (minVal <= maxVal)
+    op.count();
+    if (range_min <= range_max)
     {
-        int key = (minVal + maxVal) / 2;
-        Node* node = new Node(key);
-        node->left = BuildTreeRec(minVal, key - 1);
-        node->right = BuildTreeRec(key + 1, maxVal);
+        int data = (range_min + range_max) / 2;
+        Node* node = new Node(data);
+        node->left = BuildPBTFromRange(range_min, data - 1, op);
+        node->right = BuildPBTFromRange(data + 1, range_max, op);
+
         if (node->left != nullptr)
+        {
             node->size += node->left->size;
+            node->left->parent = node;
+            op.count(2);
+        }
         if (node->right != nullptr)
+        {
             node->size += node->right->size;
+            node->right->parent = node;
+            op.count(2);
+        }
+
+        op.count(6);
         return node;
     }
     return nullptr;
 }
 
-OSTree::Node* OSTree::OS_SELECT(OSTree::Node* root, int i)
+OSTree::Node* OSTree::FindMin(Node* node, Operation op)
 {
+    op.count();
+    if (node == nullptr)
+        return nullptr;
+
+    while (node->left != nullptr)
+    {
+        node = node->left;
+        op.count();
+    }
+    op.count();
+    return node;
+}
+
+OSTree::Node* OSTree::Select(OSTree::Node* root, int i, Operation op)
+{
+    op.count();
     if (root == nullptr)
         return nullptr;
 
     int r = 1;
+    op.count(2);
     if (root->left != nullptr)
     {
         r += root->left->size;
+        op.count();
     }
 
+    op.count();
     if (i == r)
     {
         return root;
     }
     else if (i < r)
     {
-        return OS_SELECT(root->left, i);
+        op.count();
+        return Select(root->left, i, op);
     }
-    return OS_SELECT(root->right, i - r);
+    op.count();
+    return Select(root->right, i - r, op);
 }
 
-void OSTree::DeleteTree(Node* root)
+OSTree::Node* OSTree::Delete(Node* node, Operation op)
 {
-    if (root == nullptr)
+    op.count();
+    if (node->left != nullptr && node->right != nullptr)
+    {
+        Node* successor = FindMin(node->right, op);
+        swap(node->data, successor->data);
+        node = successor;
+        op.count(5);
+    }
+
+    Node* child;
+    if (node->left != nullptr)
+        child = node->left, op.count(2);
+    else
+        child = node->right, op.count();
+
+    op.count();
+    if (child != nullptr)
+        child->parent = node->parent, op.count();;
+
+    op.count();
+    if (node->parent == nullptr)
+    {
+        root = child;
+        op.count();
+    }
+    else
+    {
+        op.count(2);
+        if (node == node->parent->left)
+            node->parent->left = child;
+        else
+            node->parent->right = child;
+    }
+    return node;
+}
+
+void OSTree::DecrementSizeOfAncestors(Node* node, Operation op)
+{
+    op.count();
+    if (node == nullptr)
         return;
-    DeleteTree(root->left);
-    DeleteTree(root->right);
-    delete root;
+    while (node->parent != nullptr)
+    {
+        node = node->parent;
+        node->size--;
+        op.count(3);
+    }
+    op.count();
 }
 
 void OSTree::PrintRec(Node* node, int indent)
@@ -125,17 +226,22 @@ void OSTree::PrintRec(Node* node, int indent)
         cout << "null\n";
     }
     else {
-        cout << node->key << "\n";
+        cout << node->data << "\n";
         PrintRec(node->left, indent + 4);
         PrintRec(node->right, indent + 4);
     }
 }
 
-OSTree::Node::Node(int key)
+OSTree::Node::Node(int data) : data(data), left(nullptr), right(nullptr), parent(nullptr), size(1)
 {
-    this->key = key;
-    this->left = this->right = nullptr;
-    this->size = 1;
+}
+
+OSTree::Node::~Node()
+{
+    if (left != nullptr)
+        delete left;
+    if (right != nullptr)
+        delete right;
 }
 
 void Demo()
@@ -143,21 +249,69 @@ void Demo()
     const int size = 11;
     const int nrOfSearches = 3;
 
-    OSTree* os = OSTree::BUILD_TREE(size);
+    Profiler profiler("Demo");
+    Operation op = profiler.createOperation("dummy", 0);
+
+    OSTree* os = OSTree::Build(size, op);
+
     os->Print();
 
     for (int i = 0; i < nrOfSearches; i++)
     {
-        int index = rand() % (size - 1) + 1;
-        cout << "Searching for \"" << index << "\". Result: \"" << os->OS_SELECT(index) << "\"\n";
+        int index = rand() % (os->Size() - 1) + 1;
+        cout << "Searching for \"" << index << "\". Result: \"" << os->Select(index, op) << "\"\n";
+        os->Delete(index, op);
+        os->Print();
     }
 
     delete os;
 }
 
+void Evaluate()
+{
+    const int range_min = 100;
+    const int range_max = 10000;
+    const int step_size = 100;
+    const int nr_of_measurements = 5;
+
+    Profiler profiler("Dynamic-Order-Statistics");
+
+    for (int m = 1; m <= nr_of_measurements; m++) {
+        for (int n = range_min; n <= range_max; n += step_size)
+        {
+            cout << "measurement: " << m << " size: " << n << "\n";
+            Operation opBuild = profiler.createOperation("BUILD_TREE", n);
+            Operation opSelect = profiler.createOperation("OS_SELECT", n);
+            Operation opDelete = profiler.createOperation("OS_DELETE", n);
+
+            OSTree* os = OSTree::Build(n, opBuild);
+
+            for (int i = 0; i < n; i++)
+            {
+                int index = os->Size() == 1 ? 1 : rand() % (os->Size() - 1) + 1;
+                os->Select(index, opSelect);
+                os->Delete(index, opDelete);
+            }
+        }
+    }
+    profiler.divideValues("BUILD_TREE", nr_of_measurements);
+    profiler.divideValues("OS_SELECT", nr_of_measurements);
+    profiler.divideValues("OS_DELETE", nr_of_measurements);
+    profiler.createGroup("ALL_OPERATIONS", "BUILD_TREE", "OS_SELECT", "OS_DELETE");
+
+    profiler.addSeries("SELECT_AND_DELETE", "OS_SELECT", "OS_DELETE");
+    profiler.addSeries("TOTAL", "SELECT_AND_DELETE", "BUILD_TREE");
+
+    profiler.showReport();
+}
+
 int main()
 {
     srand(time(NULL));
+#ifdef DEMO
     Demo();
+#else
+    Evaluate();
+#endif // DEMO
     return 0;
 }
