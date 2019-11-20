3c170ca59d5edc6d113384a10a476bbbc5400604
Katona Áron <katonaaron01@gmail.com>
2019-11-20 21:47:35 +0200

    Revert "Lab 06: Methods replaced inside the Node class"
    This reverts commit aa6aca873f13986d8f7863de7bfc69cff85685b1.

--- Lab 06/Lab 06/Lab 06.cpp
+++ Lab 06/Lab 06/Lab 06.cpp
@@ -6,38 +6,32 @@ using namespace std;
 
 class OSTree
 {
-    static class Node
+    class Node
     {
+    public:
         int key;
         Node* left;
         Node* right;
         size_t size;
 
-    public:
-
         Node(int key);
-        ~Node();
-        static Node* BuildPBTFromRange(int range_min, int range_max);
-
-        Node* OS_SELECT(int i);
-        void Delete();
-        size_t Size();
-        int GetKey();
-
-        //Demo
-        void Print(int indent);
     };
 
     Node* root;
+
     OSTree();
 public:
     static OSTree* BUILD_TREE(int n);
     int OS_SELECT(int i);
-    void OS_DELETE(int i);
     ~OSTree();
-
     //Demo
     void Print();
+private:
+    static Node* BuildTreeRec(int minVal, int maxVal);
+    Node* OS_SELECT(OSTree::Node* root, int i);
+    void DeleteTree(Node* root);
+    //Demo
+    void PrintRec(Node* node, int indent);
 };
 
 OSTree::OSTree()
@@ -50,61 +44,39 @@ OSTree* OSTree::BUILD_TREE(int n)
     if (n < 1)
         throw new invalid_argument("n must be >= 1");
     OSTree* os = new OSTree();
-    os->root = Node::BuildPBTFromRange(1, n);
+    os->root = BuildTreeRec(1, n);
     return os;
 }
 
 int OSTree::OS_SELECT(int i)
 {
-    if (root == nullptr || i < 1 || (unsigned int)i > root->Size())
+    if (root == nullptr || i < 1 || (unsigned int)i > root->size)
         throw new invalid_argument("i must be between 1 and the size of the tree");
-    Node* node = root->OS_SELECT(i);
+    Node* node = OS_SELECT(root, i);
     assert(node != nullptr);
-    return node->GetKey();
-}
-
-void OSTree::OS_DELETE(int i)
-{
-    Node* node = root->OS_SELECT(i);
-    node->Delete();
+    return node->key;
 }
 
 OSTree::~OSTree()
 {
-    if (root != nullptr)
-        delete root;
+    DeleteTree(root);
 }
 
 void OSTree::Print()
 {
     cout << "Tree:\n";
-    root->Print(0);
+    PrintRec(root, 0);
     cout << "\n";
 }
 
-OSTree::Node::Node(int key)
+OSTree::Node* OSTree::BuildTreeRec(int minVal, int maxVal)
 {
-    this->key = key;
-    this->left = this->right = nullptr;
-    this->size = 1;
-}
-
-OSTree::Node::~Node()
-{
-    if (left != nullptr)
-        delete left;
-    if (right != nullptr)
-        delete right;
-}
-
-OSTree::Node* OSTree::Node::BuildPBTFromRange(int range_min, int range_max)
-{
-    if (range_min <= range_max)
+    if (minVal <= maxVal)
     {
-        int key = (range_min + range_max) / 2;
+        int key = (minVal + maxVal) / 2;
         Node* node = new Node(key);
-        node->left = BuildPBTFromRange(range_min, key - 1);
-        node->right = BuildPBTFromRange(key + 1, range_max);
+        node->left = BuildTreeRec(minVal, key - 1);
+        node->right = BuildTreeRec(key + 1, maxVal);
         if (node->left != nullptr)
             node->size += node->left->size;
         if (node->right != nullptr)
@@ -114,52 +86,56 @@ OSTree::Node* OSTree::Node::BuildPBTFromRange(int range_min, int range_max)
     return nullptr;
 }
 
-OSTree::Node* OSTree::Node::OS_SELECT(int i)
+OSTree::Node* OSTree::OS_SELECT(OSTree::Node* root, int i)
 {
-    int r = 1 + (left == nullptr ? 0 : left->size);
+    if (root == nullptr)
+        return nullptr;
+
+    int r = 1;
+    if (root->left != nullptr)
+    {
+        r += root->left->size;
+    }
 
     if (i == r)
     {
-        return this;
+        return root;
     }
     else if (i < r)
     {
-        return left->OS_SELECT(i);
+        return OS_SELECT(root->left, i);
     }
-    return right->OS_SELECT(i - r);
+    return OS_SELECT(root->right, i - r);
 }
 
-void OSTree::Node::Delete()
+void OSTree::DeleteTree(Node* root)
 {
-    if (left == nullptr || right == nullptr)
+    if (root == nullptr)
+        return;
+    DeleteTree(root->left);
+    DeleteTree(root->right);
+    delete root;
+}
+
+void OSTree::PrintRec(Node* node, int indent)
+{
+    cout << string(indent, ' ');
+    if (node == nullptr)
     {
-        
+        cout << "null\n";
+    }
+    else {
+        cout << node->key << "\n";
+        PrintRec(node->left, indent + 4);
+        PrintRec(node->right, indent + 4);
     }
 }
 
-size_t OSTree::Node::Size()
+OSTree::Node::Node(int key)
 {
-    return this->size;
-}
-
-int OSTree::Node::GetKey()
-{
-    return this->key;
-}
-
-void OSTree::Node::Print(int indent)
-{
-    cout << string(indent, ' ') << key << "\n";
-
-    indent += 4;
-    if (left != nullptr)
-        left->Print(indent);
-    else
-        cout << string(indent, ' ') << "null" << "\n";
-    if (right != nullptr)
-        right->Print(indent);
-    else
-        cout << string(indent, ' ') << "null" << "\n";
+    this->key = key;
+    this->left = this->right = nullptr;
+    this->size = 1;
 }
 
 void Demo()
