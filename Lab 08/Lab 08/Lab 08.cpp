f7946c6562aaf3901970ff5bad7e2a5f159df53f
Katona Áron <katonaaron01@gmail.com>
2019-11-28 14:22:35 +0200

    Lab 08: Disjoint class template added

--- Lab 08/Lab 08/Lab 08.cpp
+++ Lab 08/Lab 08/Lab 08.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
+
+template <class T>
+class DisjointSet
+{
+    class Node {
+        Node* parent;
+        int rank;
+        T data;
+    };
+
+    map<T, Node*> objectsToNodes_;
+
+public:
+    DisjointSet();
+    ~DisjointSet();
+    void MakeSet(T element);
+    T FindSet(T element);
+    void Union(T x, T y);
+private:
+    Node* FindSet(Node* node);
+    void Link(Node* node1, Node* node2);
+};
+
+class Graph {
+
+};
 
 int main()
 {
