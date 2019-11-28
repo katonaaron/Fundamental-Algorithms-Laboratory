ddeb62dc61671389ec4daceba93adefab5e75b6a
Katona Áron <katonaaron01@gmail.com>
2019-11-28 16:47:23 +0200

    Lab 08: Disjoint set implemented

--- Lab 08/Lab 08/Lab 08.cpp
+++ Lab 08/Lab 08/Lab 08.cpp
@@ -1,35 +1,99 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 template <class T>
 class DisjointSet
 {
-    class Node {
-        Node* parent;
+    struct Node {
+        T parent;
         int rank;
-        T data;
     };
 
-    map<T, Node*> objectsToNodes_;
+    unordered_map<T, Node> objectsToNodes_;
 
 public:
     DisjointSet();
-    ~DisjointSet();
-    void MakeSet(T element);
-    T FindSet(T element);
-    void Union(T x, T y);
+    void MakeSet(const T& element);
+    const T& FindSet(const T& element);
+    void Union(const T& x, const T& y);
 private:
-    Node* FindSet(Node* node);
-    void Link(Node* node1, Node* node2);
+    void Link(const T& x, const T& y);
 };
 
 class Graph {
 
 };
 
+void Demo()
+{
+    DisjointSet<int> ds;
+    for (int i = 1; i <= 10; i++)
+    {
+        ds.MakeSet(i);
+    }
+    ds.Union(1, 2);
+    ds.Union(1, 3);
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "->" << ds.FindSet(i) << " ";
+    }
+    cout << "\n";
+}
+
 int main()
 {
+
+    Demo();
+
     return 0;
+}
+
+template<class T>
+DisjointSet<T>::DisjointSet()
+{
+}
+
+template<class T>
+void DisjointSet<T>::MakeSet(const T& element)
+{
+    objectsToNodes_.insert({ element, {element, 0} });
+}
+
+template<class T>
+const T& DisjointSet<T>::FindSet(const T& element)
+{
+    Node& node = objectsToNodes_.at(element);
+    if (node.parent != element)
+        node.parent = FindSet(node.parent);
+    return node.parent;
+}
+
+template<class T>
+void DisjointSet<T>::Union(const T& x, const T& y)
+{
+    Link(FindSet(x), FindSet(y));
+}
+
+template<class T>
+void DisjointSet<T>::Link(const T& x, const T& y)
+{
+    if (x == y)
+        return;
+    Node& nodeX = objectsToNodes_.at(x);
+    Node& nodeY = objectsToNodes_.at(y);
+    if (nodeX.rank > nodeY.rank)
+    {
+        nodeX.parent = y;
+    }
+    else
+    {
+        nodeY.parent = x;
+        if (nodeX.rank == nodeY.rank)
+        {
+            nodeY.rank++;
+        }
+    }
+
 }
