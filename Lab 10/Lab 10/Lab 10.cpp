1d5c2cb84face3d8cf9df4cab2e4adaba0da0162
Katona Áron <katonaaron01@gmail.com>
2019-12-18 19:12:01 +0200

    Update Lab 10.cpp

--- Lab 10/Lab 10/Lab 10.cpp
+++ Lab 10/Lab 10/Lab 10.cpp
@@ -40,6 +40,7 @@ void TopologicalSortDemo()
     const int nrVertices = 9;
     const vector<pair<int, int>> edges = {
         {0, 1}, {0, 6}, {1, 2}, {1, 6}, {2, 5}, {3, 2}, {3, 4}, {4, 5}, {7, 6} // DAG
+        //{0, 1}, {0, 6}, {1, 2}, {1, 6}, {2, 5}, {3, 2}, {3, 4}, {4, 5}, {7, 6}, {5, 0} //Not DAG
     };
 
     Graph graph(nrVertices);
